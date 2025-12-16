5ea457cbe0697f63c68dfed20b3142527dbc868b
sangyeobhan <sangyeobhandev@gmail.com>
2025-12-16 17:20:36 +0900

    primary 색 초록색 변경

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,52 +1,50 @@
 import React from 'react';
-import { Mail, GitHub } from 'react-feather';
+import {GitHub, Mail} from 'react-feather';
 
-import DecorativeSwoops from './DecorativeSwoops';
 import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <div className={styles.wrapper}>
-      <DecorativeSwoops />
-      <div className={styles.content}>
-        <div className={styles.links}>
-          <a
-            href="mailto:sangyeobhandev@gmail.com"
-            className={styles.iconLink}
-            aria-label="Email"
-          >
-            <Mail size="1.5rem" />
-          </a>
-          <a
-            href="https://github.com/sangyeobhan"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.iconLink}
-            aria-label="GitHub"
-          >
-            <GitHub size="1.5rem" />
-          </a>
-        </div>
-        {/*
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.links}>
+            <a
+                href="mailto:sangyeobhandev@gmail.com"
+                className={styles.iconLink}
+                aria-label="Email"
+            >
+              <Mail size="1.5rem"/>
+            </a>
+            <a
+                href="https://github.com/sangyeobhan"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.iconLink}
+                aria-label="GitHub"
+            >
+              <GitHub size="1.5rem"/>
+            </a>
+          </div>
+          {/*
           NOTE: If you'd like to build your blog on top
           of this code, the license requires that you leave
           this paragraph untouched. Check out LICENSE.md
           for more information.
         */}
-        <p className={styles.attribution}>
-          Blog template created by{' '}
-          <a href="https://www.joshwcomeau.com/">
-            Josh W. Comeau
-          </a>
-          . Check out{' '}
-          <a href="https://www.joyofreact.com/">
-            The Joy of React
-          </a>{' '}
-          to learn how to build dynamic React apps like
-          this one!
-        </p>
+          <p className={styles.attribution}>
+            Blog template created by{' '}
+            <a href="https://www.joshwcomeau.com/">
+              Josh W. Comeau
+            </a>
+            . Check out{' '}
+            <a href="https://www.joyofreact.com/">
+              The Joy of React
+            </a>{' '}
+            to learn how to build dynamic React apps like
+            this one!
+          </p>
+        </div>
       </div>
-    </div>
   );
 }
 
--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,8 @@ export const BLOG_TITLE = '일자산';
 
 const COLORS = {
   '--color-text': 'hsl(0deg 0% 5%)',
-  '--color-primary-500': 'hsl(245deg 100% 60%)',
-  '--color-secondary-500': 'hsl(328deg 100% 50%)',
+  '--color-primary-500': 'hsl(120deg 35% 50%)',
+  '--color-secondary-500': 'hsl(120deg 45% 70%)',
   '--color-decorative-100': 'white',
   '--color-decorative-300': 'hsl(0deg 0% 95%)',
   '--color-decorative-500': 'white',
@@ -11,16 +11,16 @@ const COLORS = {
   '--color-decorative-700': 'hsl(0deg 0% 80%)',
   '--color-decorative-900': 'hsl(0deg 0% 40%)',
   '--color-gray-0': 'white',
-  '--color-gray-300': 'hsl(50deg 10% 70%)',
-  '--color-gray-500': 'hsl(50deg 4% 50%)',
-  '--color-gray-700': 'hsl(50deg 10% 25%)',
-  '--color-gray-900': 'hsl(50deg 20% 10%)',
+  '--color-gray-300': 'hsl(95deg 12% 70%)',
+  '--color-gray-500': 'hsl(95deg 8% 50%)',
+  '--color-gray-700': 'hsl(95deg 12% 25%)',
+  '--color-gray-900': 'hsl(95deg 18% 10%)',
 };
 
 // Semantic colors
 COLORS['--color-primary'] = COLORS['--color-primary-500'];
 COLORS['--color-secondary'] = COLORS['--color-secondary-500'];
-COLORS['--color-inline-code-bg'] = 'hsl(45deg 74% 85%)';
+COLORS['--color-inline-code-bg'] = 'hsl(92deg 85% 85%)';
 COLORS['--color-selection-text'] = 'black';
 COLORS['--color-selection-background'] = COLORS['--color-decorative-700'];
 COLORS['--color-backdrop'] = COLORS['--color-decorative-500'];
@@ -32,17 +32,12 @@ COLORS['--color-card-border'] = 'transparent';
 
 const SHADOWS = {
   '--shadow-page': `
-    0px 1px 2px hsl(50deg 60% 50% / 0.25),
-    0px 3px 6px hsl(50deg 60% 50% / 0.25),
-    0px 9px 18px hsl(50deg 60% 50% / 0.25),
-    0px 18px 36px hsl(50deg 60% 50% / 0.25),
-    0px 54px 108px hsl(50deg 60% 50% / 0.25)
+    0px 2px 8px  hsl(95deg 25% 40% / 0.18),
+    0px 18px 48px hsl(95deg 25% 40% / 0.14)
   `,
   '--shadow-card': `
-    0px 1px 2px hsl(50deg 20% 50% / 0.2),
-    0px 2px 4px hsl(50deg 20% 50% / 0.2),
-    0px 4px 8px hsl(50deg 20% 50% / 0.2),
-    0px 8px 16px hsl(50deg 20% 50% / 0.2)
+    0px 1px 3px hsl(95deg 20% 40% / 0.14),
+    0px 8px 20px hsl(95deg 20% 40% / 0.10)
   `,
 };
 
