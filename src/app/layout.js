9f86faf3350cffb84245ad92493a26124d2cf206
sangyeobhan <sangyeobhandev@gmail.com>
2025-12-16 12:59:00 +0900

    5B: Animations with Motion

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
+import {Spline_Sans_Mono, Work_Sans,} from 'next/font/google';
 import clsx from 'clsx';
 
-import {LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE} from '@/constants';
+import {BLOG_TITLE, DARK_TOKENS, LIGHT_TOKENS} from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './styles.css';
+import RespectMotionPreferences from "@/components/RespectMotionPreferences";
 
 const mainFont = Work_Sans({
   subsets: ['latin'],
@@ -28,22 +26,25 @@ export const metadata = {
   title: BLOG_TITLE,
   description: '개발 기록 저장소'
 }
-function RootLayout({ children }) {
+
+function RootLayout({children}) {
   const theme = 'light';
 
   return (
-    <html
-      lang="en"
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-    >
-      <body>
-        <Header theme={theme} />
+      <RespectMotionPreferences>
+        <html
+            lang="en"
+            className={clsx(mainFont.variable, monoFont.variable)}
+            data-color-theme={theme}
+            style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+        >
+        <body>
+        <Header theme={theme}/>
         <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+        <Footer/>
+        </body>
+        </html>
+      </RespectMotionPreferences>
   );
 }
 
--- src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
+++ src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
@@ -1,8 +1,9 @@
 'use client';
 import React from 'react';
 import clsx from 'clsx';
+import {LayoutGroup, motion} from 'framer-motion';
 
-import { range } from '@/utils';
+import {range} from '@/utils';
 import Card from '@/components/Card';
 import SliderControl from '@/components/SliderControl';
 
@@ -14,86 +15,103 @@ function DivisionGroupsDemo({
   initialNumOfGroups = 1,
   includeRemainderArea,
 }) {
-  const [numOfGroups, setNumOfGroups] = React.useState(
-    initialNumOfGroups
-  );
+  const id = React.useId();
+
+  const [numOfGroups, setNumOfGroups] =
+      React.useState(initialNumOfGroups);
 
   const numOfItemsPerGroup = Math.floor(
-    numOfItems / numOfGroups
+      numOfItems / numOfGroups
   );
 
   const remainder = includeRemainderArea
-    ? numOfItems % numOfGroups
-    : null;
+      ? numOfItems % numOfGroups
+      : null;
 
-  // When we're splitting into 1-3 groups, display side-by-side
-  // columns. When we get to 4, it should switch to a 2x2 grid.
   const gridStructure =
-    numOfGroups < 4
-      ? {
-          gridTemplateColumns: `repeat(${numOfGroups}, 1fr)`,
-        }
-      : {
-          gridTemplateColumns: '1fr 1fr',
-          gridTemplateRows: '1fr 1fr',
-        };
+      numOfGroups < 4
+          ? {
+            gridTemplateColumns: `repeat(${numOfGroups}, 1fr)`,
+          }
+          : {
+            gridTemplateColumns: '1fr 1fr',
+            gridTemplateRows: '1fr 1fr',
+          };
 
   return (
-    <Card as="section" className={styles.wrapper}>
-      <header className={styles.header}>
-        <SliderControl
-          label="Number of Groups"
-          className={styles.slider}
-          step={1}
-          min={1}
-          max={4}
-          value={numOfGroups}
-          onChange={(ev) =>
-            setNumOfGroups(Number(ev.target.value))
-          }
-        />
-      </header>
+      <LayoutGroup>
+        <Card as="section" className={styles.wrapper}>
+          <header className={styles.header}>
+            <SliderControl
+                label="Number of Groups"
+                className={styles.slider}
+                step={1}
+                min={1}
+                max={4}
+                value={numOfGroups}
+                onChange={(ev) =>
+                    setNumOfGroups(Number(ev.target.value))
+                }
+            />
+          </header>
 
-      <div className={styles.demoWrapper}>
-        <div
-          className={clsx(styles.demoArea)}
-          style={gridStructure}
-        >
-          {range(numOfGroups).map((groupIndex) => (
-            <div key={groupIndex} className={styles.group}>
-              {range(numOfItemsPerGroup).map((index) => {
-                return (
+          <div className={styles.demoWrapper}>
+            <div
+                className={clsx(styles.demoArea)}
+                style={gridStructure}
+            >
+              {range(numOfGroups).map((groupIndex) => (
                   <div
-                    key={index}
-                    className={styles.item}
-                  />
-                );
-              })}
+                      key={groupIndex}
+                      className={styles.group}
+                  >
+                    {range(numOfItemsPerGroup).map(
+                        (index) => {
+                          const totalInPreviousGroups =
+                              groupIndex *
+                              numOfItemsPerGroup;
+                          const layoutId = `${id}-${
+                              index + totalInPreviousGroups
+                          }`;
+
+                          return (
+                              <motion.div
+                                  key={layoutId}
+                                  layoutId={layoutId}
+                                  className={styles.item}
+                              />
+                          );
+                        }
+                    )}
+                  </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
 
-      {includeRemainderArea && (
-        <div className={styles.remainderArea}>
-          <p className={styles.remainderHeading}>
-            Remainder Area
-          </p>
+          {includeRemainderArea && (
+              <div className={styles.remainderArea}>
+                <p className={styles.remainderHeading}>
+                  Remainder Area
+                </p>
 
-          {range(remainder).map((index) => {
-            return (
-              <div key={index} className={styles.item} />
-            );
-          })}
-        </div>
-      )}
+                {range(remainder).map((index) => {
+                  return (
+                      <div
+                          key={index}
+                          className={styles.item}
+                      />
+                  );
+                })}
+              </div>
+          )}
 
-      <Equation
-        dividend={numOfItems}
-        divisor={numOfGroups}
-        remainder={remainder}
-      />
-    </Card>
+          <Equation
+              dividend={numOfItems}
+              divisor={numOfGroups}
+              remainder={remainder}
+          />
+        </Card>
+      </LayoutGroup>
   );
 }
 
