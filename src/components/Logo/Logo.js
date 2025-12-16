030cd312dd4933f7c8dec31725a11525e591651f
sangyeobhan <sangyeobhandev@gmail.com>
2025-12-16 14:06:21 +0900

    로고 추가

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
-import { BLOG_TITLE } from '@/constants';
+import {BLOG_TITLE} from '@/constants';
 
 import styles from './Logo.module.css';
 
-function Logo({ mobileAlignment = 'left' }) {
+function Logo({mobileAlignment = 'left'}) {
   return (
-    <Link
-      href="/"
-      className={styles.wrapper}
-      data-mobile-alignment={mobileAlignment}
-    >
-      {BLOG_TITLE}
-    </Link>
+      <Link
+          href="/"
+          className={styles.wrapper}
+          data-mobile-alignment={mobileAlignment}
+      >
+        <Image
+            src="/logo.png"
+            alt="Logo"
+            width={50}
+            height={50}
+            className={styles.logoImage}
+            priority
+        />
+        <span className={styles.logoText}>{BLOG_TITLE}</span>
+        <Image
+            src="/logo.png"
+            alt="Logo"
+            width={50}
+            height={50}
+            className={styles.logoImage}
+        />
+      </Link>
   );
 }
 
